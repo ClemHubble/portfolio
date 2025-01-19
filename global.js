13761051e1b272dc93e30de9795a39457abe1209
Fong Clement Vo <clementsolar9@gmail.com>
2025-01-18 19:28:33 -0800

    lab 3 step 3

--- global.js
+++ global.js
@@ -1,13 +1,52 @@
+// console.log('IT’S ALIVE!');
+
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
+
+// const navLinks = $$("nav a");
+
+// const currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// currentLink?.classList.add('current'); 
+
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'about/', title: 'About' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/your-profile', title: 'GitHub' },
+];
 
-const currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-currentLink?.classList.add('current'); 
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  a.toggleAttribute('target', a.host !== location.host);
+
+  nav.append(a);
+}
