b4a5ccc28bfa51d96bd6cb692efd2355f8bb8cee
Fong Clement Vo <clementsolar9@gmail.com>
2025-01-19 00:09:03 -0800

    Update global.js

--- global.js
+++ global.js
@@ -4,6 +4,13 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+function getBasePath() {
+  // Get the base path depending on whether we're on GitHub Pages or local
+  const path = location.pathname;
+  const baseDir = path.substring(0, path.lastIndexOf('/'));
+  return baseDir;
+}
+
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
@@ -21,7 +28,15 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
 
-  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  // url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  if (!url.startsWith('http')) {
+    if (ARE_WE_HOME) {
+        url = url; // Keep as is for home page
+    } else {
+        // Use absolute path from root of the project
+        url = `${getBasePath()}/../${url}`;
+    }
+  }
 
   let a = document.createElement('a');
   a.href = url;
