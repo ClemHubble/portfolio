2c911e499277f6bcaa6d5f34c3151309f59d559b
Fong Clement Vo <clementsolar9@gmail.com>
2025-01-25 08:30:19 -0800

    lab 4 step 1.6

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,16 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
+// Fetch projects data
 const projects = await fetchJSON('../lib/projects.json');
 
+// Get the container to display the projects
 const projectsContainer = document.querySelector('.projects');
 
+// Render the projects
 renderProjects(projects, projectsContainer, 'h2');
+
+// Count the number of projects and update the title
+const projectsTitle = document.querySelector('.projects-title');
+if (projectsTitle) {
+  projectsTitle.innerHTML = `${projects.length} Projects`;
+}
