75461e05ca6899cb890a95484c9557860c23def7
Fong Clement Vo <clementsolar9@gmail.com>
2025-02-01 14:40:22 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -4,7 +4,6 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
-
 renderProjects(projects, projectsContainer, 'h2');
 
 const projectsTitle = document.querySelector('.projects-title');
@@ -12,35 +11,36 @@ if (projectsTitle) {
   projectsTitle.innerHTML = `${projects.length} Projects`;
 }
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length, 
+  (d) => d.year 
+);
+
+let data = rolledData.map(([year, count]) => ({
+  value: count,
+  label: year.toString()
+}));
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-
 let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data); 
+let arcData = sliceGenerator(data);
 
-let colors = d3.scaleOrdinal(d3.schemeTableau10);;
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-let arcs = arcData.map((d) => arcGenerator(d));
+let svg = d3.select('svg');
 
-arcs.forEach((arc, idx) => {
-  d3.select('svg')
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', colors(idx)) 
-})
+let arcs = arcData.map((d, idx) => {
+  svg.append('path')
+    .attr('d', arcGenerator(d))
+    .attr('fill', colors(idx));
+});
 
 let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-    legend.append('li')
-          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-})
+legend.html('');
 
+data.forEach((d, idx) => {
+  legend.append('li')
+        .attr('style', `--color:${colors(idx)}`)
+        .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+});
