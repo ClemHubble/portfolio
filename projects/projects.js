e7f80ba4142f1aa4d3f826e44e9c672931dd752e
Fong Clement Vo <clementsolar9@gmail.com>
2025-02-01 09:57:55 -0800

    lab 5 step 1.3

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,4 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
@@ -10,3 +11,10 @@ const projectsTitle = document.querySelector('.projects-title');
 if (projectsTitle) {
   projectsTitle.innerHTML = `${projects.length} Projects`;
 }
+
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
