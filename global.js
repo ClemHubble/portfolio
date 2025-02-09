8ca521372f1a972d811f1c2414453ff0e35926b9
Fong Clement Vo <clementsolar9@gmail.com>
2025-02-08 20:46:27 -0800

    lab 6 step 0.1

--- global.js
+++ global.js
@@ -18,6 +18,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'resume/', title: 'Resume' },
   { url: 'contact/', title: 'Contact' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'https://github.com/ClemHubble', title: 'GitHub' },
 ];
 
